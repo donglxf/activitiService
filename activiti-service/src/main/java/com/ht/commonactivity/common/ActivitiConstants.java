db37377855a7fe93c2a1e07cc954636a95e1dbaf
dongyoubing <dongyoubing@hongte.info>
2018-05-17 10:37:18 +0800

    首页登录修改

--- activiti-service/src/main/java/com/ht/commonactivity/common/ActivitiConstants.java
+++ activiti-service/src/main/java/com/ht/commonactivity/common/ActivitiConstants.java
@@ -44,5 +44,5 @@ public class ActivitiConstants {
     public final static String PROC_MODEL_EXCUTE_TYPE_KEY = "excuteType";// 模型运行模式流程变量key值
     public final static String PROC_TASK_ID_VAR_KEY = "proc_task_id";// 模型执行任务taskid在流程变量key值
 
-    public final static String PROC_BUSINESS_KEY = "BUSINESS_KEY";// 流程业务
+    public final static String PROC_BUSINESS_KEY = "businessKey";// 流程业务
 }
