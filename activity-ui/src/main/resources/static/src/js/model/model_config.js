5ef5bbdd0c4bd1a1fc76e07c4c614b81d09e2af0
dongyoubing <dongyoubing@hongte.info>
2018-05-23 11:48:44 +0800

    model名字修改

--- activity-ui/src/main/resources/static/src/js/model/model_config.js
+++ activity-ui/src/main/resources/static/src/js/model/model_config.js
@@ -1,5 +1,5 @@
 var pathConfig = {
       activitiConfigPath:"/activiti/extend/" // 流程引擎配置模块根路径
     , activitiServicePath:"/activity/service/" // 流程引擎配置模块根路径
-    , activitiUiPath:"/activiti/ui/"
+    , activitiUiPath:"/activity/ui/"
 }
