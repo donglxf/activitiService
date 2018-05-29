9de6fb59c70bb251bd715e152d75d71fb5069886
dongyoubing <dongyoubing@hongte.info>
2018-05-29 15:31:13 +0800

    功能完善

--- activity-service/src/main/java/com/ht/commonactivity/controller/ActivitiController.java
+++ activity-service/src/main/java/com/ht/commonactivity/controller/ActivitiController.java
@@ -724,6 +724,7 @@ public class ActivitiController implements ModelDataJsonConstants {
         byte[] b = new byte[1024];
         int len;
         while ((len = imageStream.read(b, 0, 1024)) != -1) {
+            response.setCharacterEncoding("UTF-8");
             response.getOutputStream().write(b, 0, len);
         }
 
